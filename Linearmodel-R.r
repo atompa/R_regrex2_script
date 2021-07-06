546b33388083d4db94f505288a1ff4f3f2285e1c
Abigail Tompa <tompa.1@buckeyemail.osu.edu>
2021-07-06 16:15:14 +0000

    frist commit

--- Linearmodel-R.r
+++ Linearmodel-R.r
@@ -1,34 +1,27 @@
-#!/bin/bash
-
-# Call png() with the name of the file we want to create
-png('script_r.png')
+#!/usr/bin/env Rscript
+args <- commandArgs(trailingOnly = TRUE)
 
 # Re-create the plot from the last exercise
-csv_data<-read.csv("regrex2.csv")
+csv_data <- read.csv(args[1])
+print (args[1])
 
-
+png('Scatterplot_R.png')
 plot(x = csv_data$x,
      y = csv_data$y,
      xlab = "x",
      ylab = "y",
      main = "Regrex Data")
+dev.off()
 
+png(filename="Linearplot_R.png")
 plot(x = csv_data$x,
      y = csv_data$y,
      xlab = "x",
      ylab = "y",
      col = "black",
      main = "Regrex Data")
-  
 abline(lm(csv_data$y ~ csv_data$x,
           data = csv_data), col = "black")
-
-# Save our file and return to our interactive session
 dev.off()
 
-# Verify that we have created the file
-list.files(pattern = "png")
 
- png(filename="faithful.png")
-> plot(faithful)
-> dev.off()
